08174921c3d07f411abdf61342835dcb57a4d352
Pallav Rai <143748569+youknowhim@users.noreply.github.com>
2026-04-27 14:20:39 +0530

    redis deployment changes

--- src/config/redis.js
+++ src/config/redis.js
@@ -1,17 +1,13 @@
 const Redis = require("ioredis");
 
-const redis = new Redis({
-  host: "127.0.0.1",
-  port: 6379,
-  maxRetriesPerRequest: null
-});
+const redis = new Redis(process.env.REDIS_URL);
 
 redis.on("connect", () => {
   console.log("Redis connected");
 });
 
 redis.on("error", (err) => {
-  console.log("Redis error:", err.message);
+  console.error("Redis error:", err);
 });
 
 module.exports = redis;
